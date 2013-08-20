719fdd9a8a248687f4ab1ee6a3fcf506c7c41e6d
Nell Shamrell <nshamrell@bluebox.net>
2013-08-19 20:32:03 -0700

    basic url composition, playing with method_missing

--- lib/nice_reply_client.rb
+++ lib/nice_reply_client.rb
@@ -1,5 +1,39 @@
 require "nice_reply_client/version"
 
-module NiceReplyClient
-  # Your code goes here...
+class NiceReplyClient
+  def initialize(credentials={})
+    @user_name = credentials[:user_name]
+    @api_key = credentials[:api_key]
+  end
+
+  def method_missing(method, *args)
+    compose_url(method,args)
+  end
+
+  private
+
+  def core_url
+    "http://www.nicereply.com/api/"
+  end
+
+  def credentials
+    "apikey=#{@api_key}"
+  end
+
+  def add_options(args)
+    options_string = ""
+
+    args.each do |a|
+      a.each do |k,v|
+       options_string += "&#{k}=#{v}"
+      end
+    end
+
+    options_string
+  end
+
+  def compose_url(method,args)
+    add_options(args)
+    "#{core_url}#{method}?#{credentials}#{add_options(args)}"
+  end
 end
--- spec/nice_reply_client_spec.rb
+++ spec/nice_reply_client_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe NiceReplyClient do
-  it "does something"
+  let(:user_name) { "user@something.com" }
+  let(:api_key) { "abc123" }
+
+  let :nice_reply_client do
+    NiceReplyClient.new(
+      api_key: api_key
+    )
+  end
+
+  context "forming a url" do
+    it "adds the core url" do
+      nice_reply_client.getUserAverage.should include("http://www.nicereply.com/api/")
+    end
+
+    it "adds the credentials" do
+      nice_reply_client.getUserAverage.should include("apikey=#{api_key}")
+    end
+
+    it "adds the method name" do
+      nice_reply_client.getUserAverage.should include("getUserAverage")
+    end
+
+    it "adds the options" do
+      nice_reply_client.getUserAverage(userid: 1234).should include("userid=1234")
+    end
+  end
 end
