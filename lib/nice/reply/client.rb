dd6948124dafb365a45ffa63ad4b3116f12bd8ee
Nell Shamrell <nshamrell@bluebox.net>
2013-08-19 19:46:36 -0700

    removing unnecessary files

--- lib/nice/reply/client.rb
+++
@@ -1,9 +0,0 @@
-require "nice/reply/client/version"
-
-module Nice
-  module Reply
-    module Client
-      # Your code goes here...
-    end
-  end
-end
--- lib/nice/reply/client/version.rb
+++
@@ -1,7 +0,0 @@
-module Nice
-  module Reply
-    module Client
-      VERSION = "0.0.1"
-    end
-  end
-end
